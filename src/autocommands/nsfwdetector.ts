ae80fb40bd6b4181a82fa9c9891dea99456d202e
DoormatIka <trashcanartistlily@gmail.com>
2023-04-25 09:22:27 +0800

    implementation of base filter

--- src/autocommands/nsfwdetector.ts
+++ src/autocommands/nsfwdetector.ts
@@ -2,6 +2,28 @@ import {AutoCommand} from "../types/auto.js";
 import axios from "axios";
 import tf from "@tensorflow/tfjs";
 import nsfw from "nsfwjs";
+import fs from "fs";
+import sharp from "sharp";
+
+
+const pixels = 1756 * 2357;
+const channels = 3;
+const bufferImg = fs.readFileSync("./data/letty.jpg");
+
+const img = new Int32Array(pixels * channels);
+const model = await nsfw.load();
+
+
+for (let i = 0; i < pixels; i++) {
+  for (let c = 0; c < channels; ++c) {
+    img[i * channels + c] = bufferImg[i * 4 + c];
+  }
+}
+
+const tensor = tf.tensor3d(img, [1756, 2357, 3], "int32");
+const prediction = await model.classify(tensor);
+tensor.dispose();
+console.log(prediction);
 
 export const auto: AutoCommand = {
   name: "nsfw",
