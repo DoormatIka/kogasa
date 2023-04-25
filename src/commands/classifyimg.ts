83d11f6227db0a473a39e76191f418826bb80db8
DoormatIka <trashcanartistlily@gmail.com>
2023-04-25 11:29:34 +0800

    nsfw detector implementation

---
+++ src/commands/classifyimg.ts
@@ -0,0 +1,13 @@
+import { Command } from "../types/command.js";
+
+export const command: Command = {
+  data: { 
+    name: "ping",
+    description: "Pings you, I promise!",
+    usage: "ping [user]"
+  },
+  async execute (msg) {
+    await msg.channel.sendTyping();
+    setTimeout(() => msg.channel.send("Hello there."), 1000);
+  },
+};
