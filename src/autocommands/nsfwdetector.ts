73b6f5281efed5bbf65185c32cfe4d50dd339ae0
DoormatIka <trashcanartistlily@gmail.com>
2023-04-25 08:19:50 +0800

    dependencies

--- src/autocommands/nsfwdetector.ts
+++ src/autocommands/nsfwdetector.ts
@@ -1,11 +1,14 @@
 import {AutoCommand} from "../types/auto.js";
+import axios from "axios";
+import tf from "@tensorflow/tfjs";
+import nsfw from "nsfwjs";
 
 export const auto: AutoCommand = {
   name: "nsfw",
   description: "Filters NSFW!",
   async execute (msg) {
     if (msg.author.id === "509683395224141827") {
-      msg.reply(msg.content);
+      console.log("Hi.");
     }
   },
 };
