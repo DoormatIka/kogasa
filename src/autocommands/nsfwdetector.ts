59277b5295cf9f036ff5925418cd95c94dd0cca7
DoormatIka <trashcanartistlily@gmail.com>
2023-04-25 11:29:34 +0800

    nsfw detector implementation

--- src/autocommands/nsfwdetector.ts
+++ src/autocommands/nsfwdetector.ts
@@ -1,36 +1,34 @@
-import {AutoCommand} from "../types/auto.js";
-import axios from "axios";
 import tf from "@tensorflow/tfjs";
 import nsfw from "nsfwjs";
 import fs from "fs";
 import sharp from "sharp";
 
+const img = await sharp(fs.readFileSync("./data/letty.jpg"))
+  .resize({ width: 400 })
+  .jpeg({ quality: 80 })
+  .toBuffer({ resolveWithObject: true });
 
-const pixels = 1756 * 2357;
-const channels = 3;
-const bufferImg = fs.readFileSync("./data/letty.jpg");
+console.info(`Running on ${img.info.width}x${img.info.height}`);
+const tensor = convertBufferToTensor(img.data, { x: img.info.width, y: img.info.height } );
 
-const img = new Int32Array(pixels * channels);
 const model = await nsfw.load();
-
-
-for (let i = 0; i < pixels; i++) {
-  for (let c = 0; c < channels; ++c) {
-    img[i * channels + c] = bufferImg[i * 4 + c];
-  }
-}
-
-const tensor = tf.tensor3d(img, [1756, 2357, 3], "int32");
 const prediction = await model.classify(tensor);
 tensor.dispose();
 console.log(prediction);
 
-export const auto: AutoCommand = {
-  name: "nsfw",
-  description: "Filters NSFW!",
-  async execute (msg) {
-    if (msg.author.id === "509683395224141827") {
-      console.log("Hi.");
+
+function convertBufferToTensor (
+  imageBuffer: Buffer, 
+  resolution: { x: number, y: number }, 
+  channels = 3 
+) {
+  const pixels = resolution.x * resolution.y;
+  const img = new Int32Array((pixels) * channels);
+  for (let i = 0; i < pixels; i++) {
+    for (let c = 0; c < channels; ++c) {
+      img[i * channels + c] = imageBuffer[i * 4 + c];
     }
-  },
-};
+  }
+  return tf.tensor3d(img, [resolution.x, resolution.y, 3], "int32");
+}
+
