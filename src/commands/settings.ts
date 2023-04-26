9449cd99c5430816e4e9018068009511c7cb8be1
DoormatIka <trashcanartistlily@gmail.com>
2023-04-27 01:04:20 +0800

    feat/parsee-modification

--- src/commands/settings.ts
+++ src/commands/settings.ts
@@ -25,19 +25,30 @@ export const command: Command = {
 function parsee (cmd: string): { booleanMatches: string[], optionMatches: string[][] } {
   const parsed = cmd.split(" ");
   parsed.shift();
-  const optionMatches = [];
+  const optionMatches: string[][] = [];
   const booleanMatches: string[] = [];
   for (let index = 0; index < parsed.length; index++) {
-    const parse = parsed[index]; 
-    const booleanMatch = parse.match("--");
-    if (!booleanMatch) {
-      optionMatches.push(parse.split(":"));
-      continue;
-    }
-    booleanMatches.push(parse);
+    const parse = parsed[index];
+    parseBooleanAndOption(parse, optionMatches, booleanMatches);
   }
 
   return {
     booleanMatches, optionMatches
   };
+}
+
+function parseBooleanAndOption (
+  parse: string, 
+  optionMatches: string[][], 
+  booleanMatches: string[]
+) { 
+  const booleanMatch = parse.match("--");
+  if (!booleanMatch) {
+    const splittedOptionMatches = parse.split(":");
+    if (splittedOptionMatches.length === 2) {
+      optionMatches.push(splittedOptionMatches);
+    }
+    return;
+  }
+  booleanMatches.push(parse);
 }
